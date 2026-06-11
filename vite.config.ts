adefc9355c6beefccbbe6b142cf79f97c6955db5
sabbbb0211-byte <sabbbb0211@gmail.com>
2026-06-12 01:38:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base:  '/ghcjgcjhgc/' , 
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
